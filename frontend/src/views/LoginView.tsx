c010431be44f5e8411ada8b73c2b49c3035bbe52
Aslly Zuñiga <asllyz2610@gmail.com>
2026-01-26 22:43:13 -0500

    feat: add login view with register link

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function LoginView() {
-  return <div>LoginView</div>;
+  return (
+    <>
+      <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+
+      <nav className="mt-10">
+        <Link
+          className="text-center text-white text-lg block"
+          to="/auth/register"
+        >
+          ¿No tienes cuenta? Crea una aquí
+        </Link>
+      </nav>
+    </>
+  );
 }
