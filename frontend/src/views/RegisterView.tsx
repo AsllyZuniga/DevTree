37c0cb064f139ea61cd2f6aa0e978b544a710d49
Aslly Zuñiga <asllyz2610@gmail.com>
2026-01-28 21:50:32 -0500

    feat: handle user registration with toast feedback and form reset

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import axios, { isAxiosError } from "axios";
+import { isAxiosError } from "axios";
+import { toast } from "sonner";
 import type { RegisterForm } from "../types";
 import ErrorMessage from "../components/ErrorMessage";
+import api from "../config/axios";
 
 export default function RegisterView() {
   const initialValues: RegisterForm = {
@@ -15,6 +17,7 @@ export default function RegisterView() {
   const {
     register,
     watch,
+    reset,
     handleSubmit,
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
@@ -23,16 +26,13 @@ export default function RegisterView() {
 
   const handleRegister = async (formData: RegisterForm) => {
     try {
-      const { data } = await axios.post(
-        "http://localhost:4000/auth/register",
-        formData,
-      );
-      console.log(data);
+      const { data } = await api.post(`/auth/register`, formData);
+      toast.success(data);
+      reset();
     } catch (error) {
       if (isAxiosError(error) && error.response) {
-        console.log(error.response?.data.error);
+        toast.error(error.response.data.error);
       }
-      console.log(error);
     }
   };
   return (
