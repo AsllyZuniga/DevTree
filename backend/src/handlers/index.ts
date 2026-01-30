6cb421d6c8907cae97eb580648b583b83bc050d7
Aslly Zuñiga <asllyz2610@gmail.com>
2026-01-29 22:13:17 -0500

    feat: add get authenticated user endpoint

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import slug from "slug";
+import jwt from "jsonwebtoken";
 import User from "../models/User";
 import { checkPassword, hashPassword } from "../utils/auth";
 import { generateJWT } from "../utils/jwt";
@@ -57,4 +58,33 @@ export const login = async (req: Request, res: Response) => {
   const token = generateJWT({ id: user._id });
 
   res.send(token);
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    const error = new Error("No Autorizado");
+    return res.status(401).json({ error: error.message });
+  }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    const error = new Error("No Autorizado");
+    return res.status(401).json({ error: error.message });
+  }
+
+  try {
+    const result = jwt.verify(token, process.env.JWT_SECRET);
+    if (typeof result === "object" && result.id) {
+      const user = (await User.findById(result.id)).isSelected("name handle");
+      if (!user) {
+        const error = new Error("El usuario no existe");
+        return res.status(404).json({ error: error.message });
+      }
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Token No Válido" });
+  }
 };
