4ef4ad0a74948bfef608bfbf0b9fdbbbcf0d2f94
Aslly Zuñiga <asllyz2610@gmail.com>
2026-02-14 23:42:48 -0500

    feat: configure QueryClient and wrap app with QueryClientProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {} from "@tanstack/react-query-devtools";
 import Router from "./router";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>,
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
