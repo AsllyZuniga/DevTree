80b466e205791bad968da967a56b9eaa87078b88
Aslly Zuñiga <asllyz2610@gmail.com>
2026-02-16 21:34:14 -0500

    feat: enhance JWT validation and user lookup in authenticate middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -18,28 +18,29 @@ export const authenticate = async (
   const bearer = req.headers.authorization;
 
   if (!bearer) {
-    const error = new Error("No Autorizado");
-    return res.status(401).json({ error: error.message });
+    return res.status(401).json({ error: "No Autorizado" });
   }
 
   const [, token] = bearer.split(" ");
+
   if (!token) {
-    const error = new Error("No Autorizado");
-    return res.status(401).json({ error: error.message });
+    return res.status(401).json({ error: "No Autorizado" });
   }
 
   try {
-    const result = jwt.verify(token, process.env.JWT_SECRET);
-    if (typeof result === "object" && result.id) {
-      const user = (await User.findById(result.id)).isSelected("name handle");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    if (typeof decoded === "object" && decoded.id) {
+      const user = await User.findById(decoded.id).select("-password");
+
       if (!user) {
-        const error = new Error("El usuario no existe");
-        return res.status(404).json({ error: error.message });
+        return res.status(401).json({ error: "No Autorizado" });
       }
+
       req.user = user;
       next();
     }
   } catch (error) {
-    res.status(500).json({ error: "Token No Válido" });
+    return res.status(401).json({ error: "Token No Válido" });
   }
 };
