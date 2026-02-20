c353ece2c8c88dc54d9fdeb95a2912618701909f
Aslly Zuñiga <asllyz2610@gmail.com>
2026-02-19 22:48:18 -0500

    feat: add image upload mutation for profile picture in ProfileView

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import ErrorMessage from "../components/ErrorMessage";
 import type { ProfileForm, User } from "../types";
-import { updateProfile } from "../api/DevTreeAPI";
+import { updateProfile, uploadImage } from "../api/DevTreeAPI";
 import { toast } from "sonner";
 export default function ProfileView() {
   const queryClient = useQueryClient();
@@ -28,6 +28,31 @@ export default function ProfileView() {
       queryClient.invalidateQueries({ queryKey: ["user"] });
     },
   });
+
+  const uploadImageMutation = useMutation({
+    mutationFn: uploadImage,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      queryClient.setQueryData(["user"], (prevData: User) => {
+        return {
+          ...prevData,
+          image: data,
+        };
+      });
+    },
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      console.log(e.target.files[0]);
+
+      uploadImageMutation.mutate(e.target.files[0]);
+    }
+  };
+
   const handleUserProfileForm = (formData: ProfileForm) => {
     updateProfileMutation.mutate(formData);
   };
@@ -75,7 +100,7 @@ export default function ProfileView() {
           name="handle"
           className="border-none bg-slate-100 rounded-lg p-2"
           accept="image/*"
-          onChange={() => {}}
+          onChange={handleChange}
         />
       </div>
 
